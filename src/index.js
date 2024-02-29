524a82ba4624df852a39bc3d1bf757ebff18e617
Ovidiu Postoiu <postoiu.o@gmail.com>
2024-02-29 10:55:17 +0200

    Final touch

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const pagesPath = path.join(__dirname, '/pages');
+
+function readPageData(url) {
+    try {
+        const data = fs.readFileSync(path.join(pagesPath, url), 'utf8');
+        return data;
+    } catch(error) {
+        return error;
+    }
+}
+
+const server = http.createServer((req, res) => {
+    let { url } = req;
+
+    if(url === '/') {
+        url = 'index.html';
+    }
+
+    if(url === '/favicon.ico') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    const data = readPageData(url);
+
+    if(data instanceof Error) {
+        res.writeHead(404);
+        res.end(readPageData('/404.html'));
+        return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+}).listen(8080);
